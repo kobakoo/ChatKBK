6df86859d2d933b3e63e8455ecf5590d162d1b24
Kobako <100463101+kobakoo@users.noreply.github.com>
2023-05-21 02:16:40 +0900

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -6,6 +7,8 @@ export default function Home() {
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    まだ、手抜きだお<br/>
+    チャットは<Link href="/chat" className="underline underline-offset-2 text-3xl font-bold">こちら</Link>
     </>
     )
 }
