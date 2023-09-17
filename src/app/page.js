596f36cae987e3f40dcc14ab9b49d0224ee6a55d
kobako <koba_ryusei21@hotmail.com>
2023-09-17 21:32:47 +0900

    説明文の変更

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,19 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    まだ、手抜きだお<br/>
-    チャットは<Link href="/chat" className="underline underline-offset-2 text-3xl font-bold">こちら</Link>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      チャットができるアプリだよ。　　＊画像も貼れるけど保存期間が7日敷かないから気をつけてね
+      <br />
+      チャットは
+      <Link
+        href="/chat"
+        className="underline underline-offset-2 text-3xl font-bold"
+      >
+        こちら
+      </Link>
     </>
-    )
+  );
 }
