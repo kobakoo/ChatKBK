4d46df9d516c22fa7a03846a757fc06914007e2a
kobako <koba_ryusei21@hotmail.com>
2024-02-24 00:00:59 +0900

    サイト復活「

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -78,7 +78,8 @@ function page() {
           </a>
         </div>
       </nav>
-      <div
+
+      {/* <div
         className="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 mt-4"
         role="alert"
       >
@@ -95,6 +96,24 @@ function page() {
         <div>
           <span className="font-bold">部屋の作成が行えるようになりました!</span>{" "}
           少し変更して、もう一度投稿してみてください。
+        </div>
+      </div> */}
+      <div
+        class="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+        role="alert"
+      >
+        <svg
+          class="flex-shrink-0 inline w-4 h-4 me-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span class="sr-only">Info</span>
+        <div>
+          <span class="font-medium">注意！</span> 現在一時的に部屋の作成機能を消しています。しばしお待ちください
         </div>
       </div>
       {/* <div
