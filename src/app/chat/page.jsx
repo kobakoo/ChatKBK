8030e24f1a63311b19b6dece3f83b31f3b896bed
kobako <koba_ryusei21@hotmail.com>
2024-02-24 00:27:45 +0900

    マージンの追加

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -103,7 +103,7 @@ function page() {
         </div>
       </div> */}
       <div
-        class="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+        class="flex items-center p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 mt-5"
         role="alert"
       >
         <svg
