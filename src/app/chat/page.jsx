0a2957434b2215b221ea7c4947bb699de039be2e
kobako <koba_ryusei21@hotmail.com>
2024-02-23 23:46:47 +0900

    作成ボタンの簡易削除

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -110,10 +110,9 @@ function page() {
         >
           <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
         </svg>
-        <span class="sr-only">Info</span>
+        <span class="sr-only">警告</span>
         <div>
-          <span class="font-medium">Danger alert!</span> Change a few things up
-          and try submitting again.
+          <span class="font-medium">警告！</span> 一度再読み込みしてください！
         </div>
       </div>
 
