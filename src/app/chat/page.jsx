83263fb7b1720f32ea62233a5daab1f80fb1daed
kobako <koba_ryusei21@hotmail.com>
2024-02-25 00:50:53 +0900

    軽微なUIの修正

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -323,13 +323,11 @@ function page() {
                     >
                       <path d="M480-80q-139-35-229.5-159.5T160-516v-244l320-120 320 120v244q0 152-90.5 276.5T480-80Zm0-84q104-33 172-132t68-220v-189l-240-90-240 90v189q0 121 68 220t172 132Zm0-316Z" />
                     </svg>
-                    <p className="mb:text-lg text-md font-bold flex">
+                    <p className="mb:text-lg text-md font-bold text-pretty">
                       部屋を全体に公開
-                      <p className=" font-mono font-extrabold">
-                        {showPublish
-                          ? "しない(友達で楽しく)"
-                          : "する(みんなでワイワイ)"}
-                      </p>
+                      {showPublish
+                        ? "しない(友達で楽しく)"
+                        : "する(みんなでワイワイ)"}
                     </p>
                   </div>
                   <div>
@@ -362,11 +360,9 @@ function page() {
                           <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                         </svg>
 
-                        <p className="mb:text-lg text-md font-bold flex">
+                        <p className="mb:text-lg text-md font-bold text-pretty">
                           部屋を誰でも消せるように
-                          <p className=" font-mono font-extrabold">
-                            {showDeletable ? "しない(自分だけ)" : "する"}
-                          </p>
+                          {showDeletable ? "しない(自分だけ)" : "する"}
                         </p>
                       </div>
                     ) : (
